841df822bdca5b322870125866ade8344a621278
DylanGates <solomonafoh@gmail.com>
2025-08-05 03:23:10 +0000

    Ticket Price Calculator

---
+++ Ticket Price Calculator/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class TicketPriceCalculator
+{
+    static void Main()
+    {
+        Console.Write("Please enter your age: ");
+        int age = int.Parse(Console.ReadLine());
+
+        double price = (age <= 12 || age >= 65) ? 7.0 : 10.0;
+
+        Console.WriteLine($"Your ticket price is: GHS{price}");
+    }
+}
